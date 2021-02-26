99ad8a597f372d96c85eb3a562818bcc71290557
Luke Geneva <lukegeneva@gmail.com>
2021-02-26 00:31:26 -0600

    Add support for bold text

--- test/wordToMarkdown.test.js
+++ test/wordToMarkdown.test.js
@@ -9,12 +9,18 @@ describe("wordToMarkdown", () => {
   it("should convert a simple text string", async () => {
     const filePath = resolveTestFilePath("plain-text.docx");
     const output = await wordToMarkdown(filePath);
-    assert.equal(output, "Some plain text.\n");
+    assert.equal(output, "Some plain text.");
   });
 
   it("should convert multiple paragraphs", async () => {
     const filePath = resolveTestFilePath("two-paragraphs.docx");
     const output = await wordToMarkdown(filePath);
-    assert.equal(output, "Two.\n\nParagraphs.\n");
+    assert.equal(output, "Two.\n\nParagraphs.");
+  });
+
+  it("should print bold text", async () => {
+    const filePath = resolveTestFilePath("bold-text.docx");
+    const output = await wordToMarkdown(filePath);
+    assert.equal(output, "*Bold text.*");
   });
 });
--- wordToMarkdown.js
+++ wordToMarkdown.js
@@ -1,20 +1,44 @@
 const fetchDocument = require("./fetchDocument");
 
-const elementToText = (element, markdown = "") => {
-  let childText = "";
-  if (!element) return markdown;
-  if (element.elements)
-    childText = element.elements.map((e) => elementToText(e)).join("");
-  let elementText = "";
-  if (element.type === "text") elementText = element.text;
-  if (element.name === "w:p") elementText = "\n";
-  return childText + elementText;
+const renderDocument = (document) => {
+  const documentNode = document.elements.find((e) => e.name === "w:document");
+  const body = documentNode.elements.find((e) => e.name === "w:body");
+  return renderBody(body);
+};
+
+const renderBody = (body) => {
+  const paragraphs = body.elements.filter((e) => e.name === "w:p");
+  return paragraphs.map(renderParagraph).join("\n");
+};
+
+const renderParagraph = (paragraph) => {
+  const runs = paragraph.elements.filter((e) => e.name === "w:r");
+  return runs.map(renderRun).join("");
+};
+
+const renderRun = (run) => {
+  const tElement = run.elements.find((e) => e.name === "w:t");
+  if (!tElement) return "";
+  const textElement = tElement.elements.find((e) => e.type === "text");
+  const text = textElement ? textElement.text : "";
+  return renderText(getStyle(run), text);
+};
+
+const renderText = (style, text) => {
+  if (style.isBold) return `*${text}*`;
+  return text;
+};
+
+const getStyle = (run) => {
+  const rPr = run.elements.find((e) => e.name === "w:rPr");
+  const isBold = !!rPr.elements.find((e) => e.name === "w:b");
+  return { isBold };
 };
 
 const wordToMarkdown = async (filePath) => {
   return new Promise((resolve, reject) => {
     fetchDocument(filePath).then((doc) => {
-      const markdown = elementToText(doc);
+      const markdown = renderDocument(doc);
       resolve(markdown);
     });
   });
